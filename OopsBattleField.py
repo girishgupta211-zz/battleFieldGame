c2ed78e6215fc207bdb02609ae8de24597f654f5
Girish Gupta <girishgargdce@gmail.com>
2017-06-17 20:36:09 +0530

    Update OopsBattleField.py

--- OopsBattleField.py
+++ OopsBattleField.py
@@ -1,16 +1,13 @@
 import string
 from pprint import pprint 
 
-def validateShipRange(func):
+def validateBattleShip(func):
 	def inner_func(self,location, dimention, type,maxX,maxY ):		
 		x =ord(location[0]) - 64
 		y = location[1]		
 
 		if ( x+dimention[0]-1 > maxX or  y+dimention[1]-1 > ord(maxY) - 64):
-			raise Exception("tanks should be within battle field dimention of " + str(maxX) + " by " + str(maxY))
-
-		# if (type )
-
+			raise Exception("battleShip should be within battle field dimention of " + str(maxX) + " by " + str(maxY))
 	
 		return func(self,location,dimention,type,maxX,maxY)
 	return inner_func
@@ -27,7 +24,7 @@ def validateRange(func):
 	return inner_func
 
 
-class BattleArea(object):
+class BattleArea():
 	@validateRange
 	def __init__(self,maxX,maxY):		
 		self.maxX = maxX
@@ -37,24 +34,22 @@ class BattleArea(object):
 		self.battleArea = [ [0 for i in xrange(self.maxX)] for i in xrange(self.maxY) ]
 
 
-class Ship(BattleArea):
-	@validateShipRange
+class BattleShip():
+	@validateBattleShip
 	def __init__(self, location, dimention,type,maxX,maxY):				
 		self.location = location
 		self.dimention = dimention
-		self.type = type
-		BattleArea.__init__(self,maxX, maxY)
-
+		self.type = type		
 
 class BattleField(BattleArea):
 	totalPower = 0
-	def __init__(self, m,n,tanksArry):
+	def __init__(self, m,n,battleShipArry):
 		BattleArea.__init__(self,m,n)
 		self.createBattleArea()		
-		self.populateBattleField(tanksArry)
+		self.populateBattleField(battleShipArry)
 	
-	def populateBattleField(self,tanksArry):		
-		for tank in tanksArry:
+	def populateBattleField(self,battleShipArry):		
+		for tank in battleShipArry:
 			x = tank.location[1]
 			y =ord(tank.location[0]) - 64			
 			xRange = [ x + i  for i in range(tank.dimention[0]) ]
@@ -67,6 +62,15 @@ class BattleField(BattleArea):
 		# pprint.pprint (self.battleArea)
 
 
+class Player():
+	"""docstring for Player"""
+	def __init__(self, battleField,targetMissiles):
+		self.battleField = battleField
+		self.targetMissiles = targetMissiles
+		
+def convertToCartesian(targetLocations,m,n):
+	return [ (ord(location[0]) - 64 , location[1]) for location in targetLocations]
+
 def run():
 	m = 9
 	n = 'H'
@@ -74,68 +78,62 @@ def run():
 	dimention = (1,2)
 	type = 'P'
 	
-	tanksBattleArea1 = []
-	tanksBattleArea1.append(Ship(location,dimention,type,m,n))
-	# tanksBattleArea1.append(Ship( ('H', 7) ,(2,2),'Q',m,n))	
-	battleField1 = BattleField(m,n,tanksBattleArea1)
+	battleShipsArea1 = []
+	battleShipsArea1.append(BattleShip(location,dimention,type,m,n))
+	# battleShipsArea1.append(BattleShip( ('H', 7) ,(2,2),'Q',m,n))	
+	battleField1 = BattleField(m,n,battleShipsArea1)
 
-	tanksBattleArea2 = []
-	tanksBattleArea2.append(Ship(location,dimention,type,m,n))
-	# tanksBattleArea2.append(Ship( ('D', 3) ,(2,2),'Q',m,n))	
-	battleField2 = BattleField(m,n,tanksBattleArea2)
+	battleShipsArea2 = []
+	battleShipsArea2.append(BattleShip(location,dimention,type,m,n))
+	# battleShipsArea2.append(BattleShip( ('D', 3) ,(2,2),'Q',m,n))	
+	battleField2 = BattleField(m,n,battleShipsArea2)
+	targetsPlayer1 = [('A',1) , ('A',3),  ('A',2) ]
+	targetsPlayer2 = [('A',1) , ('B',2), ('B',2), ('B',3) ]	
+	targetsPlayer1 = convertToCartesian(targetsPlayer1,m,n)
+	targetsPlayer2 = convertToCartesian(targetsPlayer2,m,n)
+	player1 = Player(battleField1,targetsPlayer1)
+	player2 = Player(battleField2,targetsPlayer2)
+	
+	play(player1, player2)	
 
-	targetsPlayer1 = [(1,1) , (1,3),  (1,2) ]
-	targetsPlayer2 = [(1,1) , (2,2) , (2,2), (2,3)]
-	play(battleField1,battleField2,targetsPlayer1,targetsPlayer2)	
 
-def hitMissile(battleField1,target):
-	# pprint (battleField1.battleArea)	
-	# pprint (battleField1.totalPower)
-	# pprint (battleField1.battleArea[target[0]][target[1]])
-	# check if target shell is an active shell( having power (1 or 2))
-	# pprint (battleField1.totalPower)	
-	if(battleField1.battleArea[target[0]-1][target[1]-1] > 0):
-		battleField1.battleArea[target[0]-1][target[1]-1] -= 1
-		battleField1.totalPower -= 1
+def hitMissile(battleField,target):	
+	# check if target shell is an active shell( having power (1 or 2))	
+	if(battleField.battleArea[target[0]-1][target[1]-1] > 0):
+		battleField.battleArea[target[0]-1][target[1]-1] -= 1
+		battleField.totalPower -= 1
 		return True
 	else:
 		return False
 
-
-	# pprint (battleField1.battleArea)
-	# pprint (battleField1.totalPower)
-
-	# print target
-
-	pass
-
-def play(battleField1,battleField2,targetsPlayer1,targetsPlayer2):	
+def play(player1,player2):	
+# def play(battleField1,battleField2,targetsPlayer1,targetsPlayer2):	
 	currPlayer = 1 
 	status = False
 	while(True):
-		if(battleField1.totalPower == 0):
+		if(player1.battleField.totalPower == 0):
 			print ("Player-2 won the battle")
 			break
-		if(battleField2.totalPower == 0):
+		if(player2.battleField.totalPower == 0):
 			print ("Player-1 won the battle")
 			break
 
-		if(len(targetsPlayer1) == 0 and len(targetsPlayer2) == 0):
+		if(len(player1.targetMissiles) == 0 and len(player2.targetMissiles) == 0):
 			print ("Player-1 and Player-2 have no more missiles left. players declare peace.")
 			break
 
-		if(len(targetsPlayer1) == 0):
+		if(len(player1.targetMissiles) == 0):
 			print ("Player-1 no more missiles left")
 			currPlayer = 2			
 
-		if(len(targetsPlayer2) == 0):
+		if(len(player2.targetMissiles) == 0):
 			print ("Player-2 no more missiles left")
 			currPlayer = 1			
 
 		# print targetsPlayer1
 		if(currPlayer == 1):
-			target = targetsPlayer1.pop(0)			
-			status = hitMissile(battleField2, target)
+			target = player1.targetMissiles.pop(0)			
+			status = hitMissile(player2.battleField, target)
 			if(status):
 				print ("Player-1 fires a missile with target " +  str(target) + " which hit")
 				currPlayer = 1				
@@ -144,8 +142,8 @@ def play(battleField1,battleField2,targetsPlayer1,targetsPlayer2):
 				currPlayer = 2
 			
 		else:	
-			target = targetsPlayer2.pop(0)			
-			status = hitMissile(battleField1, target)
+			target = player2.targetMissiles.pop(0)			
+			status = hitMissile(player1.battleField, target)
 			if(status):
 				print ("Player-2 fires a missile with target " +  str(target) + " which hit")
 				currPlayer = 2			
